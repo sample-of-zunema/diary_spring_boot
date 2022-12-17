878b0baa7ef68ce2e172c4053b9120a21f66e537
zunema <yxxa.abc@gmail.com>
2022-12-18 04:33:28 +0900

    dv接続

---
+++ src/main/java/com/example/diary/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.example.diary.service;
+
+import java.util.ArrayList;
+
+public interface MemberService {
+
+    String greet(int i); //挨拶を返す
+
+    // ArrayList<Member> getAll(); //全てのMemberを返す
+    
+}
---
+++ src/main/java/com/example/diary/service/MemberServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.diary.service;
+
+import java.util.ArrayList;
+
+public class MemberServiceImpl implements MemberService {
+
+    @Override
+    public String greet(int i) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    // @Override
+    // public ArrayList<Member> getAll() {
+    //     // TODO Auto-generated method stub
+    //     return null;
+    // }
+    
+}
