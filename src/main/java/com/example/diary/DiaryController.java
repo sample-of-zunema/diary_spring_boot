6b1ad52d232626b65563bffda85edfe115092962
zunema <yxxa.abc@gmail.com>
2022-12-29 00:12:00 +0900

    fix

--- src/main/java/com/example/diary/DiaryController.java
+++ src/main/java/com/example/diary/DiaryController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("diary")
 public class DiaryController {
     
+    
     @Autowired
     DiaryRepository diaryRepository;
 
